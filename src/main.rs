10a272c999f2da024b610758bc38c857d96f021e
Stanislav Ageev <stanislav@roomd.net>
2018-05-05 02:33:53 +0300

    CPU struct with properties to access 8 bit registers

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,65 @@
+mod bitwise;
+
+pub struct LR35902 {
+    a: u8,
+    f: u8,
+    bc: u16,
+    de: u16,
+    hl: u16,
+    sp: u16,
+    pc: u16
+}
+
+impl LR35902 {
+    pub fn get_b(&self) -> u8 {
+        bitwise::get_least(self.bc)
+    }
+
+    pub fn set_b(&self, to: u8) {
+        bitwise::set_least(self.bc, to);
+    }
+
+    pub fn get_c(&self) -> u8 {
+        bitwise::get_most(self.bc)
+    }
+
+    pub fn set_c(&self, to: u8) {
+        bitwise::set_most(self.bc, to);
+    }
+
+    pub fn get_d(&self) -> u8 {
+        bitwise::get_least(self.de)
+    }
+
+    pub fn set_d(&self, to: u8) {
+        bitwise::set_least(self.de, to);
+    }
+
+    pub fn get_e(&self) -> u8 {
+        bitwise::get_most(self.de)
+    }
+
+    pub fn set_e(&self, to: u8) {
+        bitwise::set_most(self.de, to);
+    }
+
+    pub fn get_h(&self) -> u8 {
+        bitwise::get_least(self.de)
+    }
+
+    pub fn set_h(&self, to: u8) {
+        bitwise::set_least(self.de, to);
+    }
+
+    pub fn get_l(&self) -> u8 {
+        bitwise::get_most(self.de)
+    }
+
+    pub fn set_l(&self, to: u8) {
+        bitwise::set_most(self.de, to);
+    }
+}
+
 fn main() {
+
 }
