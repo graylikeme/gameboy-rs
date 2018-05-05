e7f8c1785d16e90c9933f2a09b8e4f353af5c96e
Stanislav Ageev <stanislav@roomd.net>
2018-05-05 03:43:14 +0300

    Change binary to hex

--- src/bitwise.rs
+++ src/bitwise.rs
@@ -1,5 +1,5 @@
 pub fn set_least(of: u16, to: u8) -> u16 {
-    (of & 0b1111111100000000) | to as u16
+    (of & 0xFF00) | to as u16
 }
 
 pub fn get_least(of: u16) -> u8 {
@@ -7,7 +7,7 @@ pub fn get_least(of: u16) -> u8 {
 }
 
 pub fn set_most(of: u16, to: u8) -> u16 {
-    (of & 0b0000000011111111) | (to as u16) << 8
+    (of & 0x00FF) | (to as u16) << 8
 }
 
 pub fn get_most(of: u16) -> u8 {
