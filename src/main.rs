4db6e4c71b1ee80b589f9371829161395c6cb7e9
Stanislav Ageev <stanislav@roomd.net>
2018-05-05 03:20:54 +0300

    Fix 8bit registers mappings

--- src/main.rs
+++ src/main.rs
@@ -12,51 +12,51 @@ pub struct LR35902 {
 
 impl LR35902 {
     pub fn get_b(&self) -> u8 {
-        bitwise::get_least(self.bc)
-    }
-
-    pub fn set_b(&self, to: u8) {
-        bitwise::set_least(self.bc, to);
-    }
-
-    pub fn get_c(&self) -> u8 {
         bitwise::get_most(self.bc)
     }
 
-    pub fn set_c(&self, to: u8) {
+    pub fn set_b(&self, to: u8) {
         bitwise::set_most(self.bc, to);
     }
 
+    pub fn get_c(&self) -> u8 {
+        bitwise::get_least(self.bc)
+    }
+
+    pub fn set_c(&self, to: u8) {
+        bitwise::set_least(self.bc, to);
+    }
+
     pub fn get_d(&self) -> u8 {
-        bitwise::get_least(self.de)
+        bitwise::get_most(self.de)
     }
 
     pub fn set_d(&self, to: u8) {
-        bitwise::set_least(self.de, to);
+        bitwise::set_most(self.de, to);
     }
 
     pub fn get_e(&self) -> u8 {
-        bitwise::get_most(self.de)
-    }
-
-    pub fn set_e(&self, to: u8) {
-        bitwise::set_most(self.de, to);
-    }
-
-    pub fn get_h(&self) -> u8 {
         bitwise::get_least(self.de)
     }
 
-    pub fn set_h(&self, to: u8) {
+    pub fn set_e(&self, to: u8) {
         bitwise::set_least(self.de, to);
     }
 
-    pub fn get_l(&self) -> u8 {
+    pub fn get_h(&self) -> u8 {
         bitwise::get_most(self.de)
     }
 
-    pub fn set_l(&self, to: u8) {
+    pub fn set_h(&self, to: u8) {
         bitwise::set_most(self.de, to);
+    }
+
+    pub fn get_l(&self) -> u8 {
+        bitwise::get_least(self.de)
+    }
+
+    pub fn set_l(&self, to: u8) {
+        bitwise::set_least(self.de, to);
     }
 }
 
