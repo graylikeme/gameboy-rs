cd8006b9eba2bffeb88c22aa9af00378767d91c7
Stanislav Ageev <stanislav@roomd.net>
2018-05-05 06:03:18 +0300

    Bit enum and get_bit

--- src/bitwise.rs
+++ src/bitwise.rs
@@ -2,6 +2,13 @@ use std::mem::size_of;
 use num_traits::Unsigned;
 use num_traits::int::PrimInt;
 use num_traits::cast::NumCast;
+use num_traits::identities::Zero;
+
+#[derive(Debug)]
+pub enum Bit {
+    Zero,
+    One
+}
 
 pub fn set_least(of: u16, to: u8) -> u16 {
     (of & 0xFF00) | to as u16
@@ -27,7 +34,25 @@ pub fn flip_bit<T>(at: usize, of: T) -> Option<T>
 
     let num: Option<T> = NumCast::from(0x01);
     if  let Some(n) = num {
-        return Some((n << at) ^ of)
+        return Some((n << at) ^ of);
+    }
+
+    None
+}
+
+pub fn get_bit<T>(at: usize, of: T) -> Option<Bit>
+    where T: Unsigned + PrimInt + NumCast {
+    if size_of::<T>()*8 <= at {
+        return None
+    }
+
+    let num: Option<T> = NumCast::from(0x01);
+    if let Some(n) = num {
+        if  ((n << at) & of).is_zero() {
+            return Some(Bit::Zero);
+        }
+
+        return Some(Bit::One);
     }
 
     None
