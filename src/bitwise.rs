853886a1554c0eec32df2b47a4b7bca1ffa0bc46
Stanislav Ageev <stanislav@roomd.net>
2018-05-06 01:11:27 +0300

    Add allow dead_code annotations and remove useless import

--- src/bitwise.rs
+++ src/bitwise.rs
@@ -2,7 +2,6 @@ use std::mem::size_of;
 use num_traits::Unsigned;
 use num_traits::int::PrimInt;
 use num_traits::cast::NumCast;
-use num_traits::identities::Zero;
 
 #[derive(Debug)]
 pub enum Bit {
@@ -10,22 +9,27 @@ pub enum Bit {
     One
 }
 
+#[allow(dead_code)]
 pub fn set_least(of: u16, to: u8) -> u16 {
     (of & 0xFF00) | to as u16
 }
 
+#[allow(dead_code)]
 pub fn get_least(of: u16) -> u8 {
     of as u8
 }
 
+#[allow(dead_code)]
 pub fn set_most(of: u16, to: u8) -> u16 {
     (of & 0x00FF) | (to as u16) << 8
 }
 
+#[allow(dead_code)]
 pub fn get_most(of: u16) -> u8 {
     (of >> 8) as u8
 }
 
+#[allow(dead_code)]
 pub fn flip_bit<T>(at: usize, of: T) -> Option<T>
     where T: Unsigned + PrimInt + NumCast {
     if size_of::<T>() * 8 <= at {
@@ -36,6 +40,7 @@ pub fn flip_bit<T>(at: usize, of: T) -> Option<T>
     num.map(|n| (n << at) ^ of)
 }
 
+#[allow(dead_code)]
 pub fn get_bit<T>(at: usize, of: T) -> Option<Bit>
     where T: Unsigned + PrimInt + NumCast {
     if size_of::<T>() * 8 <= at {
@@ -48,6 +53,7 @@ pub fn get_bit<T>(at: usize, of: T) -> Option<Bit>
     })
 }
 
+#[allow(dead_code)]
 pub fn set_bit<T>(at: usize, of: T) -> Option<T>
     where T: Unsigned + PrimInt + NumCast {
     if size_of::<T>() * 8 <= at {
@@ -58,6 +64,7 @@ pub fn set_bit<T>(at: usize, of: T) -> Option<T>
     num.map(|n| (n << at) | of)
 }
 
+#[allow(dead_code)]
 pub fn unset_bit<T>(at: usize, of: T) -> Option<T>
     where T: Unsigned + PrimInt + NumCast {
     if size_of::<T>() * 8 <= at {
