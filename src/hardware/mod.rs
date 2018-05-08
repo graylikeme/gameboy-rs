2bf23e1cef138df018f6535120bf744735d68988
Stanislav Ageev <stanislav@roomd.net>
2018-05-08 07:13:57 +0300

    Create a dummy bus

---
+++ src/hardware/mod.rs
@@ -0,0 +1,17 @@
+pub mod mmu;
+
+use self::mmu::GameboyMemory;
+
+pub struct Bus {
+    mmu: Box<GameboyMemory>,
+}
+
+impl Bus {
+    pub fn new(mmu: Box<GameboyMemory>) -> Bus {
+        return Bus { mmu }
+    }
+
+    pub fn read_mem(&self, addr: u16) -> u8 {
+        self.mmu.read(addr)
+    }
+}
